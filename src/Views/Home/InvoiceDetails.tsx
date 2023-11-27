4fb7ad1463ff757ba9ea65bb4e89cf502ae3ad6b
Andrei <andrei_king1@yahoo.com>
2023-11-27 20:44:19 +0200

    Continuing with the design for invoices details

--- src/Views/Home/InvoiceDetails.tsx
+++ src/Views/Home/InvoiceDetails.tsx
@@ -6,9 +6,27 @@ export default function InvoiceDetails() {
   return (
     <>
       <div className="invoiceDetails">
-        <h3>Status : Sent</h3>
-        <h4>Invoice number: #{invoice}</h4>
-        Details will be here
+        <div className="statusSide">
+          <span>Status : Sent</span> <button>Export</button>
+        </div>
+        <div className="detailsSide">
+          <div className="invoice">
+            <h4>Invoice : {invoice}</h4>
+            <div className="labelValue">
+              <label>Invoice Number</label>
+              {invoice}
+            </div>
+            <div className="labelValue">
+              <label>Issue Date</label>
+              24 / 12 / 2023
+            </div>
+          </div>
+          <div className="client">
+            <h4>Client Electron</h4>
+            <p>244 W Ashland Ave Street, Test Louiseville, new Year station 245, by the bridges</p>
+          </div>
+        </div>
+        <div>More info will be here</div>
       </div>
     </>
   )
